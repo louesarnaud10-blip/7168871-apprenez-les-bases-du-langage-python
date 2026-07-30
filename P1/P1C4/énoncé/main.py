23870941983d738978a95cd58474b1a10f163dd9
louesarnaud10-blip <louesarnaud10@gmail.com>
2026-07-30 12:07:34 +0000

    Add variables for name and age with print statements

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+#Créez une variable nommée  nom  et attribuez-y votre nom.
+nom = "Loues"
+
+#Créez une variable nommée  age  et attribuez-y votre âge.
+age = 28
+
+#Affichez une phrase contenant votre nom et votre âge en utilisant les variables que vous venez de créer. Par exemple, "Je m'appelle Jean et j'ai 25 ans."
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+
+#Modifiez la valeur de la variable  age  pour ajouter 10 ans à votre âge actuel.
+age = 38
+
+#Affichez à nouveau la phrase contenant votre nom et votre âge en utilisant les variables mises à jour. Par exemple, "Je m'appelle Jean et j'ai 35 ans maintenant."
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.") 
