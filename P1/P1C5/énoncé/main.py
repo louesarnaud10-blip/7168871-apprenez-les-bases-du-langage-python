b6f2bf8bfc3539d2011829998c3ee721246674ae
louesarnaud10-blip <louesarnaud10@gmail.com>
2026-07-30 13:07:30 +0000

    Add variables for name, age, height, and student status

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,23 @@
-# Écrivez votre code ici !
+# Definissez une variable nom contenant votre nom sous forme de chaine de caracteres.
+nom = "loues"
+
+# Definissez une variable age contenant votre age sous forme d'un entier.
+age = 28
+
+# Definissez une variable taille contenant votre taille sous forme d'un nombre a virgule flottante.
+taille = 1.82
+
+# Definissez une variable est_etudiant contenant une valeur booléenne et indiquant si vous etes étudiant ou non.
+est_etudiant = True 
+
+# Utilisez la fonction print pour afficher les valeurs des variables precedemment definies.
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+
+Utilisez la fonction type pour afficher les types des variables précédemment définies.
+type(nom) 
+  type(age)
+type(taille)
+  type(est_etudiant) 
