3f4e72b419a238d0fb3a27953244d5f4a065488f
louesarnaud10-blip <louesarnaud10@gmail.com>
2026-07-30 10:52:24 +0000

    Implement print statements for message and calculation
    Added print statements to display a message and calculate a result.

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,5 @@
-## Écrivez votre code ici !
+#Utilisez la fonction print pour afficher la phrase `"J'apprends Python !"`
+print ("J'apprends Python !")
+
+#Calculez le résultat de17 + 35 * 2 et affichez le résultat.
+print((17 + 35)* 2)
