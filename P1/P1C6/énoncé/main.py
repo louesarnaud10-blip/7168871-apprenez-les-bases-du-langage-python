5c665a0a818d2f4c1770508fc982b3aee480065a
louesarnaud10-blip <louesarnaud10@gmail.com>
2026-07-30 19:17:02 +0000

    Implement fruit list manipulation in main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+# Créez une liste nommée  fruits  contenant les éléments  pomme  ,  banane  et  orange
+Fruits = ["pomme", "banane", "orange"] 
+
+# Ajoutez  kiwi  à la liste  fruits  
+Fruits.append("kiwi")
+
+# Supprimez  orange  de la liste  fruits  
+Fruits.remove(orange)
+
+# Modifiez le deuxième élément de la liste  fruits  en  ananas  
+Fruits[1] = "ananas"
+
+# Affichez la longueur de la liste  fruits  
+len(Fruits)
+
+# Fruits.sort()
