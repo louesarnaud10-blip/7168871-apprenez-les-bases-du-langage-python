8f2b0f5e22c6b9373dcfa299020c3c7da5c1f903
louesarnaud10-blip <louesarnaud10@gmail.com>
2026-07-30 10:35:36 +0000

    Fix print statement and add calculation in main.py
    Corrected the print statement and added a calculation.

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,4 @@
-## Écrivez votre code ici !
+#Utilisez la fonction print pour afficher la phrase `"J'apprends Python !"`
+print("J'apprends Python !")
+#Calculez le résultat de 17 + 35 * 2 et affichez le résultat
+print(17 + 35) * 2 
